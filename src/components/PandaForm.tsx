e366a55332d545b57cbd7373177caacdb0cd2b5e
Olivier Thierry <olivier.thierry@gmail.com>
2022-06-18 14:44:38 +0200

    Migrate to React Hook Form 7.x

--- src/components/PandaForm.tsx
+++ src/components/PandaForm.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'reactstrap';
 import TextInput from './TextInput';
@@ -13,14 +12,14 @@ export type PandaFormValues = {
 export type PandaFormProps = {
   initialValues?: PandaFormValues;
   onCancel(): void;
-  onSubmit(values: any): void;
+  onSubmit(values: PandaFormValues): void;
 };
 
 const PandaForm = (props: PandaFormProps) => {
   const { onCancel, onSubmit, initialValues } = props;
   const { t } = useTranslation();
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<PandaFormValues>({
@@ -29,32 +28,54 @@ const PandaForm = (props: PandaFormProps) => {
   });
   return (
     <form noValidate onSubmit={handleSubmit(onSubmit)}>
-      <TextInput
-        innerRef={register({
+      <Controller
+        name="name"
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label={t('pandaForm.name.label')}
+            placeholder={t('pandaForm.name.placeholder')}
+            error={errors.name?.message}
+            {...field}
+          />
+        )}
+        rules={{
           required: {
             value: true,
             message: t('pandaForm.name.errors.required'),
           },
-        })}
-        name="name"
-        label={t('pandaForm.name.label')}
-        placeholder={t('pandaForm.name.placeholder')}
-        error={errors.name?.message}
+        }}
       />
-      <TextInput
-        innerRef={register({
+      <Controller
+        name="interests"
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label={t('pandaForm.interests.label')}
+            placeholder={t('pandaForm.interests.placeholder')}
+            error={errors.interests?.message}
+            {...field}
+          />
+        )}
+        rules={{
           required: {
             value: true,
             message: t('pandaForm.interests.errors.required'),
           },
-        })}
-        name="interests"
-        label={t('pandaForm.interests.label')}
-        placeholder={t('pandaForm.interests.placeholder')}
-        error={errors.interests?.message}
+        }}
       />
-      <TextInput
-        innerRef={register({
+      <Controller
+        name="image"
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label={t('pandaForm.image.label')}
+            placeholder={t('pandaForm.image.placeholder')}
+            error={errors.image?.message}
+            {...field}
+          />
+        )}
+        rules={{
           required: {
             value: true,
             message: t('pandaForm.image.errors.required'),
@@ -63,11 +84,7 @@ const PandaForm = (props: PandaFormProps) => {
             value: /^https?:\/\/.*/,
             message: t('pandaForm.image.errors.invalid'),
           },
-        })}
-        name="image"
-        label={t('pandaForm.image.label')}
-        placeholder={t('pandaForm.image.placeholder')}
-        error={errors.image?.message}
+        }}
       />
       <Button color="primary" style={{ marginRight: 10 }} disabled={!isValid}>
         {t('common.submit')}
