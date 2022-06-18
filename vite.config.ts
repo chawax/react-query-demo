8d20ae2e7584ca06be01cb79113bf8251d0a933a
Olivier Thierry <olivier.thierry@gmail.com>
2022-06-18 16:23:05 +0200

    Prevent warning about top level "this"

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    define: {
+      // To prevent warning "Top-level "this" will be replaced with undefined since this file is an ECMAScript module"
+      this: 'window',
+    },
+  },
 });
