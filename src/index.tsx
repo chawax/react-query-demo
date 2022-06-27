57f64ee8b36d6d17e2e30f5f7bcb1209ec21313a
Olivier Thierry <olivier.thierry@gmail.com>
2022-06-27 20:26:48 +0200

    Fix React 18 startup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './i18n';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
