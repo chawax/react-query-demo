7f92c209fd83c5b740d19829f7a9469721637557
Olivier Thierry <olivier.thierry@gmail.com>
2023-03-04 23:01:34 +0100

    Remove Jest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,7 +4,6 @@
 import '@testing-library/jest-dom/extend-expect';
 
 // Pour empêcher les traces de console.error de Axios pourrir la sortie des tests
-
 const nativeConsoleError = global.console.error;
 global.console.error = (...args) => {
   if (args.join('').includes('Error: Network Error')) {
